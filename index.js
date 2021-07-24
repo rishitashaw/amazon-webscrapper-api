b2b64999f0a65d149e8bd22cf1ee96eefdf7e1fd
Theseregrets <rishitashaw49@gmail.com>
2021-07-24 13:21:33 +0530

    api key hidden

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+
 const express = require('express')
 const request = require('request-promise')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-const apiKey= '62fadce8fbc6f5ea7aba449b5765f9de'
+const apiKey= process.env.API_KEY
 const baseUrl=`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json())
@@ -19,7 +20,7 @@ app.get('/products/:productId', async(req, res) => {
 
     try {
         const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
-        res.json(response)
+        res.json(JSON.parse(response))
 
     } catch (error) {
         res.json(error)
