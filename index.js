b793ea85eacd6b8187220b157f28de83eb1cd4aa
Theseregrets <rishitashaw49@gmail.com>
2021-07-24 13:33:23 +0530

    dynamic api key created

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const request = require('request-promise')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-const apiKey= process.env.API_KEY
-const baseUrl=`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+//const apiKey= process.env.API_KEY
+const generateScraperUrl=(apiKey)=>`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json())
 
@@ -17,9 +17,52 @@ app.get('/',(req, res)=>{
 //get product details
 app.get('/products/:productId', async(req, res) => {
     const {productId} = req.params;
+    const {api_key}=req.query
 
     try {
-        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`)
+        res.json(JSON.parse(response))
+
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//get product reviews
+app.get('/products/:productId/reviews', async(req, res) => {
+    const {productId} = req.params;
+    const {api_key}=req.query
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`)
+        res.json(JSON.parse(response))
+
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//get product offers
+app.get('/products/:productId/offers', async(req, res) => {
+    const {productId} = req.params;
+    const {api_key}=req.query
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+        res.json(JSON.parse(response))
+
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+//get search query
+app.get('/:searchQuery', async(req, res) => {
+    const {searchQuery} = req.params;
+    const {api_key}=req.query
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`)
         res.json(JSON.parse(response))
 
     } catch (error) {
